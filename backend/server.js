5efcf45269953c1a6b6667cd82a238e2b28a3a73
MariaWellander <maria.wellander@gmail.com>
2023-01-05 20:41:03 +0100

    added installations (FE & BE), register endpoint (BE), schema & model

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from 'crypto';
+import bcrypt from 'bcrypt';
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-final-project";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -16,10 +18,58 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+});
+
+const User = mongoose.model('User', userSchema)
+
+// Routes start here
+
+// The main endpoint
 app.get("/", (req, res) => {
   res.send("Hello Welly user!");
 });
+
+// The endpoint to register
+app.post("/register", async (req, res) => {
+  const {username, password} = req.body
+  try {
+    const salt = bcrypt.genSaltSync();
+    if (password.length < 4) {
+      res.status(400).json({
+        success: false,
+        response: "Your password must be longer than 4 characters."
+      });
+    } else {
+      const newUser = await new User({
+        username: username,
+        password: bcrypt.hashSync(password, salt)
+      }).save();
+      res.status(201).json({
+        success: true,
+        response: {username: newUser.username, accessToken: newUser.accessToken, id: newUser._id}
+      })
+    }
+  } catch(error) {
+    res.status(400).json({
+      success: false,
+      response: "Error"
+    })
+  }
+})
 
 // Start the server
 app.listen(port, () => {
