ada9bad33e998fa71f5c6bf03a495c4639d069dd
MariaWellander <maria.wellander@gmail.com>
2023-01-06 15:11:21 +0100

    mongodb compass and postman works correctly

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import crypto from 'crypto';
 import bcrypt from 'bcrypt';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-final-project";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/final-project";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
